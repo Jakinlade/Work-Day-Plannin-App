be4535c50b54c6e9ee16e720d4b91a62e2bc9d3c
Jakinlade <jj.47.johnson@gmail.com>
2023-01-20 19:01:03 +0000

    added js file and link to in in html. added psuedocode for js file

---
+++ starter/style.js
@@ -0,0 +1,15 @@
+// Create a variable to store current date and time using the Moment.js library
+
+// Display the current date and time at the top of calendar
+
+// Create timeblocks for standard business hours using a for loop
+
+// Within the for loop, create a color-coding system to distinguish between past, present, and future tblocks
+
+// Allow a user to enter an event by clicking on a tblock and display the input field within that tblock
+
+// Create a save button within each tblock and use an event listener to save the entered event to local storage when clicked
+
+// Use another event listener to retrieve the saved events from local storage and display them on the calendar upon page refresh
+
+// Ensure that the events are persisted between refreshes of a page
